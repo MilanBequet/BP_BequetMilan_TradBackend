ccbd61be00dc6d663f19ac23fd67de50ea36efc4
Milan Bequet <milan.bequet@student.hogent.be>
2025-05-15 11:25:47 +0200

    Correcte databank connectie gemaakt

--- db.js
+++ db.js
@@ -1,10 +1,35 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  dialectModule: require('mysql2'),
-  logging: false,
-});
+let sequelize;
 
-module.exports = sequelize;
+async function initializeSequelize() {
+  if (sequelize) return sequelize;
+
+  const {
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME
+  } = process.env;
+
+  sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false,
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log("Verbinding met de database is geslaagd!");
+  } catch (err) {
+    console.error("Kan geen verbinding maken met de database:", err);
+    throw err;
+  }
+
+  return sequelize;
+}
+
+module.exports = initializeSequelize;
